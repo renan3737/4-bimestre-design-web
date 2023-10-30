a24e3780b2067a803082fe8b92846e933d4038ee
EA - Renan De Oliveira Nascimento <renan_nascimento@educadventista.org>
2023-10-30 15:01:13 -0300

    recebendo os parametros na rota

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 
+const caminhoBase = path.join(__dirname, "templates")
+
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Estou funcionando com o express")
+    resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, () => {
