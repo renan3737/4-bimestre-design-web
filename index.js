46ea52e0e4d84e300c24eed83e8d14b7d876e48b
EA - Renan De Oliveira Nascimento <renan_nascimento@educadventista.org>
2023-11-13 14:50:43 -0300

    kjdhds

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
 app.post('/cadastrar/salvar', (requisicao, resposta) => {
-    
+    const nome = requisicao.body.nome
+    const email = requisicao.body.email
+    const senha = requisicao.body.senha
+
+    console.log(`O email do usuário é: ${email}`)
 })
 
 app.get('/cadastrar', (requisicao, resposta) => {
